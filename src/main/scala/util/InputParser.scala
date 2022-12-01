95fba6b2345cffff2c99864d3601781a31047523
Paweł Cembaluk <pawel.cembaluk@gmail.com>
2022-12-01 21:48:14 +0100

    Simplify resources structure.

--- src/main/scala/util/InputParser.scala
+++ src/main/scala/util/InputParser.scala
@@ -9,7 +9,7 @@ abstract class InputParser[T](day: Int) {
   protected def parse(string: String): T
 
   lazy val parsedInput: T = {
-    val fileName = s"input/day$day/puzzle.txt"
+    val fileName = s"input/day$day.txt"
     val inputString = Using(Source.fromResource(fileName))(_.mkString).get
     parse(inputString)
   }
