777f442f9f72bf8505fcf990b070d2fa3a52c7ef
Paweł Cembaluk <pawel.cembaluk@gmail.com>
2022-12-01 21:46:36 +0100

    Simplify utils.

--- src/main/scala/util/InputParser.scala
+++ src/main/scala/util/InputParser.scala
@@ -1,13 +1,16 @@
 package io.github.avapl
 package util
 
+import scala.io.Source
+import scala.util.Using
 import scala.util.matching.Regex
 
 abstract class InputParser[T](day: Int) {
   protected def parse(string: String): T
 
   lazy val parsedInput: T = {
-    val inputString = FileReader.readUnsafe(s"input/day$day/puzzle.txt")
+    val fileName = s"input/day$day/puzzle.txt"
+    val inputString = Using(Source.fromResource(fileName))(_.mkString).get
     parse(inputString)
   }
 }
@@ -15,13 +18,13 @@ abstract class InputParser[T](day: Int) {
 object InputParser {
 
   implicit class InputStringOps(val string: String) extends AnyVal {
-    def splitBy(delimiter: String): Seq[String] = {
-      val quotedDelimiter = Regex.quote(delimiter)
-      string.split(quotedDelimiter).toSeq
-    }
-
     def splitByRegex(regex: String): Seq[String] =
       string.split(regex).toSeq
+
+    def splitBy(delimiter: String): Seq[String] = {
+      val quotedDelimiter = Regex.quote(delimiter)
+      splitByRegex(quotedDelimiter)
+    }
 
     def splitLines: Seq[String] =
       splitBy("\n")
