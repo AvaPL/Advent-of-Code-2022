01101373db22e03f39c3d9dd9f3489f46bf8b00d
Paweł Cembaluk <pawel.cembaluk@gmail.com>
2022-12-08 17:53:53 +0100

    Clean up day 7.

--- src/main/scala/day7/Puzzle1.scala
+++ src/main/scala/day7/Puzzle1.scala
@@ -3,6 +3,9 @@ package day7
 
 object Puzzle1 extends App {
   val fileSystem = PuzzleInputParser.parsedInput
-  val result = fileSystem.keySet.toSeq.map(directorySize(fileSystem)).filter(_ <= 100_000).sum
+  val result = fileSystem.keySet.toSeq
+    .map(path => directorySize(fileSystem)(path))
+    .filter(_ <= 100_000)
+    .sum
   println(result)
 }
--- src/main/scala/day7/Puzzle2.scala
+++ src/main/scala/day7/Puzzle2.scala
@@ -5,9 +5,13 @@ object Puzzle2 extends App {
   val fileSystem = PuzzleInputParser.parsedInput
   val totalDiskSpace = 70_000_000
   val requiredFreeSpace = 30_000_000
-  val fileSystemSize = directorySize(fileSystem)(Vector("/"))
+  val fileSystemSize = directorySize(fileSystem)(pathToCheck = Vector("/"))
   val currentFreeSpace = totalDiskSpace - fileSystemSize
   val spaceToFreeUp = requiredFreeSpace - currentFreeSpace
-  val result = fileSystem.keySet.toSeq.map(directorySize(fileSystem)).sorted.find(_ > spaceToFreeUp).get
+  val result = fileSystem.keySet.toSeq
+    .map(path => directorySize(fileSystem)(path))
+    .sorted
+    .find(_ > spaceToFreeUp)
+    .get
   println(result)
 }
