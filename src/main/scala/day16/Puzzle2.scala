2fdef08cb0582c36098909a6ece026e33d28e967
Paweł Cembaluk <pawel.cembaluk@gmail.com>
2022-12-20 22:26:06 +0100

    Clean up day 16.

--- src/main/scala/day16/Puzzle2.scala
+++ src/main/scala/day16/Puzzle2.scala
@@ -5,13 +5,11 @@ object Puzzle2 extends App {
   val valves = PuzzleInputParser.parsedInput
   val shortestPaths = floydWarshall(valves)
   val valvesWithPositiveFlowRate = valves.filter(_.flowRate > 0)
-  val valvesCombinations = LazyList.from {
-    for {
-      combinationSize <- 1 until valvesWithPositiveFlowRate.size
-      myValves <- valvesWithPositiveFlowRate.combinations(combinationSize)
-      elephantValves = valvesWithPositiveFlowRate.diff(myValves)
-    } yield (myValves, elephantValves)
-  }
+  val valvesCombinations = for {
+    combinationSize <- LazyList.from(1).takeWhile(_ < valvesWithPositiveFlowRate.size)
+    myValves <- valvesWithPositiveFlowRate.combinations(combinationSize)
+    elephantValves = valvesWithPositiveFlowRate.diff(myValves)
+  } yield (myValves, elephantValves)
   val result = valvesCombinations.map { case (myValves, elephantValves) =>
     val myPressure = simulate(myValves, shortestPaths, simulationMinutes = 26)
     val elephantPressure = simulate(elephantValves, shortestPaths, simulationMinutes = 26)
