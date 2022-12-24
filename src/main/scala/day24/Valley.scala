05c096826266a3fbef0013b01c20fda096167239
Paweł Cembaluk <pawel.cembaluk@gmail.com>
2022-12-24 22:42:28 +0100

    Clean up day 24 solution.

--- src/main/scala/day24/Valley.scala
+++ src/main/scala/day24/Valley.scala
@@ -4,11 +4,8 @@ package day24
 import scala.collection.mutable
 import scala.math.floorMod
 
-class Valley(
-    width: Int,
-    height: Int,
-    blizzards: Map[Position, Seq[Blizzard]]
-) {
+class Valley(width: Int, height: Int, blizzards: Map[Position, Seq[Blizzard]]) {
+
   val entrancePosition: Position = Position(-1, 0)
   val exitPosition: Position = Position(height, width - 1)
 
@@ -36,30 +33,33 @@ class Valley(
   private def nextValidPositions(positionsBatch: Set[Position], batchPathLength: Int) = {
     val nextBlizzardPositions = blizzardPositions(batchPathLength + 1)
     positionsBatch.flatMap { position =>
-      val adjacentPositions = Set(
-        position.copy(row = position.row - 1),
-        position.copy(row = position.row + 1),
-        position.copy(column = position.column - 1),
-        position.copy(column = position.column + 1)
-      )
-      (adjacentPositions + position).filter(isPositionValid).diff(nextBlizzardPositions)
+      (adjacentPositions(position) + position).filter(isPositionValid).diff(nextBlizzardPositions)
     }
   }
 
+  private def blizzardPositions(pathLength: Int) = {
+    for {
+      (Position(row, column), blizzards) <- blizzards
+      Blizzard(rowDirection, columnDirection) <- blizzards
+    } yield {
+      val newRow = floorMod(row + rowDirection * pathLength, height)
+      val newColumn = floorMod(column + columnDirection * pathLength, width)
+      Position(newRow, newColumn)
+    }
+  }.toSet
+
+  private def adjacentPositions(position: Position) =
+    Set(
+      position.copy(row = position.row - 1),
+      position.copy(row = position.row + 1),
+      position.copy(column = position.column - 1),
+      position.copy(column = position.column + 1)
+    )
+
   private def isPositionValid(position: Position) = {
     val Position(row, column) = position
-    0 <= row && row < height &&
-    0 <= column && column < width ||
+    0 <= row && row < height && 0 <= column && column < width ||
     position == entrancePosition ||
     position == exitPosition
   }
-
-  private def blizzardPositions(pathLength: Int) =
-    blizzards.flatMap { case (Position(row, column), blizzards) =>
-      blizzards.map { blizzard =>
-        val newRow = floorMod(row + blizzard.rowDirection * pathLength, height)
-        val newColumn = floorMod(column + blizzard.columnDirection * pathLength, width)
-        Position(newRow, newColumn)
-      }
-    }.toSet
 }
